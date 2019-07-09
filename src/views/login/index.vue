24db5f906c043c018cb3a4d97b1b387ac320d475
wangshuai157512 <wangshuai157512@163.com>
2019-07-09 09:28:13 +0800

    使用async await 完成登录页面请求

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,61 @@
 <template>
   <div>
-    <p>Login Components</p>
+    <div class="login-wrap">
+    <van-nav-bar title="标题"/>
+    <form>
+      <van-cell-group>
+        <van-field
+          v-model="user.mobile"
+          required
+          clearable
+          label="手机号"
+          placeholder="请输入手机号"
+        />
+
+        <van-field
+          v-model="user.code"
+          type="password"
+          label="密码"
+          placeholder="请输入密码"
+          required
+        />
+      </van-cell-group>
+      <div class="login-btn">
+        <van-button class="btn" type="info" @click.prevent="handleLogin" >登录</van-button>
+      </div>
+    </form>
+  </div>
   </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
-  name: 'Login'
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    async handleLogin () {
+      const res = await axios({
+        method: 'POST',
+        url: 'http://toutiao.course.itcast.cn/app/v1_0/authorizations',
+        data: this.user
+      })
+      console.log(res)
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
+.login-btn {
+  padding: 10px;
+  .btn {
+    width: 100%;
+  }
+}
 </style>
